1455da2bbab33efc21a6338a711d0b1ffe13f399
juliab <julia.siroshtan@gmail.com>
2022-12-16 19:05:16 -0500

    Authentication models added Email/password validation implemented

---
+++ lib/domain/core/value_objects.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+
+import 'failures.dart';
+
+abstract class ValueObject<T> {
+  const ValueObject();
+  Either<ValueFailure<T>, T> get value;
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+    return o is ValueObject<T> && o.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  String toString() => 'Value($value)';
+}
