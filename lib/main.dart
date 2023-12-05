f23d715ad2bfbb18d152ad5c9cfb709b81a69784
Julia Siroshtan <babenko.julia@gmail.com>
2023-12-05 10:33:13 -0500

    Android support added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:listzen/firebase_options.dart';
 import 'package:listzen/injection.dart';
 import 'package:listzen/presentation/core/app_widget.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await configureDependencies();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(AppWidget());
 }
