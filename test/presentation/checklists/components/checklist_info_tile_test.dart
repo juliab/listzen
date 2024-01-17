34b991afd9713ceb129fe8665c66dac85a987ce9
Julia Siroshtan <babenko.julia@gmail.com>
2024-01-17 15:05:14 -0500

    Divide ChecklistInfoTile widget tests into two files

--- test/presentation/checklists/components/checklist_info_tile_test.dart
+++ test/presentation/checklists/components/checklist_info_tile_test.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:listzen/domain/auth/value_objects.dart';
-import 'package:listzen/domain/checklists/checklist.dart';
 import 'package:listzen/domain/checklists/checklist_color.dart';
-import 'package:listzen/domain/checklists/item.dart';
-import 'package:listzen/domain/checklists/value_objects.dart';
 import 'package:listzen/presentation/checklists/components/checklist_info_tile.dart';
-import 'package:listzen/presentation/checklists/components/checklist_statistics.dart';
 import 'package:listzen/presentation/checklists/components/completion_status_checkbox.dart';
 
 void main() {
@@ -53,39 +48,6 @@ void main() {
       expect(find.text(checklistName), findsOneWidget);
       expect(find.byType(TextFormField), findsNothing);
       expect(find.byType(Checkbox), findsOneWidget);
-    });
-
-    testWidgets("Checklist completion statistics is displayed correctly",
-        (tester) async {
-      // Test checklist contains 15 items. 8 of them are marked as completed
-      const checklistName = 'Orange checklist';
-      final checklist = Checklist(
-        id: UniqueId(),
-        name: ChecklistName(checklistName),
-        color: ChecklistColor.orange,
-        items: List<Item>.generate(
-          15,
-          (i) => Item(
-            id: UniqueId(),
-            name: ItemName('Item $i'),
-            done: i.isEven,
-          ),
-        ),
-      );
-
-      // Build the widget
-      await tester.pumpWidget(MaterialApp(
-        home: Scaffold(
-          body: ChecklistInfoTile.readOnly(
-            color: checklist.color,
-            name: checklistName,
-            statistics: ChecklistStatistics(checklist: checklist),
-          ),
-        ),
-      ));
-
-      // Verify statistics is displayed correclty
-      expect(find.text('8 / 15'), findsOneWidget);
     });
   });
 }
