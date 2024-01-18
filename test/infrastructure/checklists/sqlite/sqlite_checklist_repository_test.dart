efd575d6f5ebc870399568d1fb88d1f23d9633b1
Julia Siroshtan <babenko.julia@gmail.com>
2024-01-18 12:43:35 -0500

    Update checklist repository test to use test data generator

--- test/infrastructure/checklists/sqlite/sqlite_checklist_repository_test.dart
+++ test/infrastructure/checklists/sqlite/sqlite_checklist_repository_test.dart
@@ -1,15 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:listzen/domain/auth/value_objects.dart';
-import 'package:listzen/domain/checklists/checklist.dart' as d;
-import 'package:listzen/domain/checklists/checklist_color.dart';
 import 'package:listzen/domain/checklists/checklist_failure.dart';
-import 'package:listzen/domain/checklists/value_objects.dart';
 import 'package:listzen/infrastructure/checklists/sqlite/sqlite_checklist_repository.dart';
 import 'package:listzen/infrastructure/checklists/sqlite/sqlite_database.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
+import '../../../data/test_data_generator.dart';
 import 'sqlite_checklist_repository_test.mocks.dart';
 
 @GenerateMocks([SqliteDatabase])
@@ -23,12 +20,7 @@ void main() {
   });
 
   group('create method tests', () {
-    final checklist = d.Checklist(
-      id: UniqueId(),
-      name: ChecklistName("Test checklist"),
-      color: ChecklistColor.blue,
-      items: [],
-    );
+    final checklist = TestDataGenerator.basicChecklist();
 
     test('Should return unit if record was created successfully', () async {
       // arrange
